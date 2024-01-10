5323fa661cea89b2ed4a6b4e4ddc5a7b19d5f4fa
danzel ong <danzelongwj01@gmail.com>
2024-01-10 21:10:09 +0800

    feat (personal details): implemented form validation functionality and styles

--- multi-step-form/src/components/form/PersonalDetails.jsx
+++ multi-step-form/src/components/form/PersonalDetails.jsx
@@ -1,7 +1,10 @@
+import ErrorMsg from '../error/ErrorMsg';
 import styles from '../../styles/PersonalDetails.module.css';
 
 export default function PersonalDetails({ formDetails, dispatch }) {
-    const { name, email, phoneNumber } = formDetails;
+    const { name, email, phoneNumber, inputStatus } = formDetails;
+    const { nameStatus, emailStatus, phoneNumberStatus } = inputStatus;
+
     return (
         <div>
             <section className={styles.userDetails}>
@@ -14,39 +17,78 @@ export default function PersonalDetails({ formDetails, dispatch }) {
                 <div className={styles.formInput}>
                     <label htmlFor='name'>Name</label>
                     <input
+                        className={`${
+                            nameStatus !== 'valid' && nameStatus
+                                ? styles.invalid
+                                : ''
+                        } ${nameStatus === 'valid' ? styles.valid : ''}`}
                         type='text'
                         name='name'
                         placeholder='e.g. Stephen King'
-                        value={name === null || !name ? '' : name}
+                        value={name || ''}
                         onChange={(e) =>
                             dispatch({ type: 'name', payload: e.target.value })
                         }
                     />
+                    {nameStatus === 'empty' && (
+                        <ErrorMsg
+                            style='form'
+                            content='This field is required'
+                        />
+                    )}
+                    {nameStatus === 'contain-special' && (
+                        <ErrorMsg
+                            style='form'
+                            content='No digits or special characters'
+                        />
+                    )}
+                    {nameStatus === 'invalid-length' && (
+                        <ErrorMsg style='form' content='Minimum of 3 letters' />
+                    )}
                 </div>
                 <div className={styles.formInput}>
                     <label htmlFor='email'>Email Address</label>
                     <input
-                        type='email'
+                        className={`${
+                            emailStatus !== 'valid' && emailStatus
+                                ? styles.invalid
+                                : ''
+                        } ${emailStatus === 'valid' ? styles.valid : ''}`}
+                        type='text'
                         name='email'
                         placeholder='e.g stephenking@lorem.com'
-                        value={email === null || !email ? '' : email}
+                        value={email || ''}
                         onChange={(e) =>
-                            dispatch({ type: 'email', payload: e.target.value })
+                            dispatch({
+                                type: 'email',
+                                payload: e.target.value,
+                            })
                         }
                     />
+                    {emailStatus === 'empty' && (
+                        <ErrorMsg
+                            style='form'
+                            content='This field is required'
+                        />
+                    )}
+                    {emailStatus === 'invalid' && (
+                        <ErrorMsg style='form' content='Invalid email format' />
+                    )}
                 </div>
                 <div className={styles.formInput}>
                     <label htmlFor='number'>Phone Number</label>
                     <input
+                        className={`${
+                            phoneNumberStatus !== 'valid' && phoneNumberStatus
+                                ? styles.invalid
+                                : ''
+                        } ${phoneNumberStatus === 'valid' ? styles.valid : ''}`}
                         inputMode='numeric'
                         type='text'
                         name='email'
                         placeholder='e.g +1 234 567 890'
-                        value={
-                            phoneNumber === null || !phoneNumber
-                                ? ''
-                                : phoneNumber
-                        }
+                        maxLength={10}
+                        value={phoneNumber || ''}
                         onChange={(e) =>
                             dispatch({
                                 type: 'phoneNumber',
@@ -54,6 +96,15 @@ export default function PersonalDetails({ formDetails, dispatch }) {
                             })
                         }
                     />
+                    {phoneNumberStatus === 'empty' && (
+                        <ErrorMsg
+                            style='form'
+                            content='This field is required'
+                        />
+                    )}
+                    {phoneNumberStatus === 'invalid' && (
+                        <ErrorMsg style='form' content='Must have 10 digits' />
+                    )}
                 </div>
             </form>
         </div>
