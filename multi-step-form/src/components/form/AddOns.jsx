96a5744c33ca7861aa25a1291d56a4cc7e783978
danzel ong <danzelongwj01@gmail.com>
2024-01-12 00:02:16 +0800

    feat (add ons): implemeted add on component

---
+++ multi-step-form/src/components/form/AddOns.jsx
@@ -0,0 +1,62 @@
+import { pickAddOns } from '../../utils/data';
+import styles from '../../styles/AddOns.module.css';
+
+export default function AddOns({ formDetails, dispatch }) {
+    const { addOns, billByMonth } = formDetails;
+
+    return (
+        <div>
+            <section className={styles.addOnTitle}>
+                <span className={styles.addOnHeading}>Pick add-ons</span>
+                <span className={styles.addOnSubHeading}>
+                    Add-ons help enhance your gaming experience
+                </span>
+            </section>
+            <section>
+                <ul className={styles.addOnList}>
+                    {pickAddOns.map((addOn) => {
+                        return (
+                            <li
+                                className={`${styles.addOn} ${
+                                    addOns[`addOn${addOn.index}`]
+                                        ? styles.active
+                                        : ''
+                                }`}
+                                key={addOn.index}
+                                onClick={() =>
+                                    dispatch({
+                                        type: 'addOns',
+                                        payload: addOn.index,
+                                    })
+                                }
+                            >
+                                <input
+                                    type='checkbox'
+                                    checked={addOns[`addOn${addOn.index}`]}
+                                    readOnly
+                                />
+                                <div className={styles.addOnContent}>
+                                    <div className={styles.addOnDetails}>
+                                        <span className={styles.addOnName}>
+                                            {addOn.name}
+                                        </span>
+                                        <span className={styles.addOnDesc}>
+                                            {addOn.desc}
+                                        </span>
+                                    </div>
+                                </div>
+                                <span className={styles.addOnPrice}>
+                                    +$
+                                    {billByMonth
+                                        ? addOn.monthlyPrice
+                                        : addOn.yearlyPrice}
+                                    {billByMonth ? '/mo' : '/yr'}
+                                </span>
+                            </li>
+                        );
+                    })}
+                </ul>
+            </section>
+        </div>
+    );
+}
